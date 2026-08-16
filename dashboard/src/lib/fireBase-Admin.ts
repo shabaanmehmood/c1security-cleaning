e1809d24a35110feeb73a876b41b64193d3d5a57
zayanwaris7-hash <zayanwaris7@gmail.com>
2026-08-16 22:51:51 +0500

    Fix

--- dashboard/src/lib/fireBase-Admin.ts
+++ dashboard/src/lib/fireBase-Admin.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase-admin/auth"; // Must be from 'firebase-admin/auth'
 
 const privateKey = process.env.FIREBASE_PRIVATE_KEY
   ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
